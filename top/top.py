5d4133f6379cf885ac2a2dc494d2701a0d48182d
Holger Berger <holger.berger@googlemail.com>
2015-08-13 00:21:45 +0200

    top like current stats top like aggregated stats

--- top/top.py
+++ top/top.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import time
+import time,sys
 
 # CONFIG
 DBHOST="localhost"
@@ -85,9 +85,10 @@ class filesystem(object):
 	# get a tuple of current timestamp and a dict of nodestats of all nodes doing IO at the moment
 	def currentNodesstats(self):
 		nodes={}
-		timestamp=fs.getLatestTs()
+		timestamp=self.getLatestTs()
 		for e in self.getEntries(timestamp):
 			node = e["nid"]
+			if node == "aggr": continue
 			if node not in nodes:
 				nodes[node]=nodestats(node)
 				nodes[node].dt = e['dt']
@@ -100,9 +101,11 @@ class filesystem(object):
 				nodes[node].rbw =   e['v'][3]
 		return (timestamp, nodes)
 
+
 	# map a dict of nodestats to running jobs at the time
 	def mapNodesToJobs(self, timestamp, nodes):
 		# { "_id" : ObjectId("559e8c86580eb358815b87a2"), "end" : 1436430906, "cmd" : "", "jobid" : "659096.intern2-2015", "nids" : "n151001", "start" : 1436425679, "owner" : "ppb742", "calc" : -1 }
+
 		# FIXME -1 = running
 		jobs={}
 		nidtojob={}
@@ -118,28 +121,116 @@ class filesystem(object):
 					jobs[jobid].owner = j["owner"]
 					jobs[jobid].cmd = j["cmd"]
 					
-					
+		# joblist contains now list of jobs of ALL clusters!!!				
+
+		fsjobs=set()
+
 		for node in nodes:
 			try:
-				jobs[nidtojob[node]].addnode(nodes[node])
+				job = nidtojob[node]
+				jobs[job].addnode(nodes[node])
+				if job not in fsjobs:
+					fsjobs.add(job)
 			except KeyError:
 				jobs[node]=jobstats(node)
 				jobs[node].addnode(nodes[node])
 				jobs[node].nodelist.append(node)
-				
+				if node not in fsjobs:
+					fsjobs.add(node)
 			
+		localjobs={}
+		for j in fsjobs:
+			localjobs[j]=jobs[j]
+		return localjobs
+			
+	# get all running jobs (from all clusters, can not be avoided)
+	def getRunningJobs(self):
+		jobs={}
+		for j in self.jobcoll.find({"end":-1}):
+			jobid=j["jobid"]
+			if jobid not in jobs:
+				jobs[jobid]=jobstats(jobid)
+				for nid in j["nids"].split(","):
+					jobs[jobid].nodelist.append(nid)
+					jobs[jobid].start = j["start"]
+					jobs[jobid].end = j["end"]
+					jobs[jobid].owner = j["owner"]
+					jobs[jobid].cmd = j["cmd"]
 		return jobs
 
+	# go over all jobs in list, and add all stats of nodes in job from start to end
+	# to it (end==-1 is covered, so can be used for running jobs as well)
+	def accumulateJobStats(self, jobs):
+		fsjobs=set()
+		for j in jobs:
+			for nid in jobs[j].nodelist:
+				if jobs[j].end == -1:
+					end = time.time()
+				else:
+					end = jobs[j].end
+				start = jobs[j].start
 
-fs = filesystem(DBHOST, "nobnec")
+				for e in self.perfcoll.find({"$and": [{"nid": nid}, {"ts": {"$gt": start}}, {"ts": {"$lt": end}} ] }):
+					node = e["nid"]
+					if node == "aggr": continue
+					if 'mdt' in e:
+						jobs[j].miops += e['v']
+					elif 'ost' in e:
+						jobs[j].wiops += e['v'][0]
+						jobs[j].wbw   += e['v'][1]
+						jobs[j].riops += e['v'][2]
+						jobs[j].rbw   += e['v'][3]
+					fsjobs.add(j)
 
-(timestamp, nodes) = fs.currentNodesstats()
-jobs = fs.mapNodesToJobs(timestamp, nodes)
+		localjobs={}
+		for j in fsjobs:
+			localjobs[j]=jobs[j]
+		return localjobs
 
-print "JOBID      OWNER    NODES  MIOPS   WIOS      WBW   RIOPS      RBW"
-print "                                             MB/s             MB/s"
-print "=================================================================="
-for j in sorted(jobs.values(), key=lambda x: x.miops, reverse=True):
-	dt = float(j.dt)
-	if j.jobid != "aggr":
+
+# print TOP like list of jobs, with current rates
+def printTopjobs(fsname, key):
+	fs = filesystem(DBHOST, fsname)
+	(timestamp, nodes) = fs.currentNodesstats()
+	jobs = fs.mapNodesToJobs(timestamp, nodes)
+	if key == "meta":
+		sortf=lambda x: x.miops	
+	elif key == "iops":
+		sortf=lambda x: x.wiops+x.riops
+	elif key == "bw":
+		sortf=lambda x: x.rbw+x.wbw
+	else:
+		print "use meta, iops or bw as sorting key"
+		sys.exit()
+	print "JOBID      OWNER    NODES  MIOPS   WIOPS     WBW   RIOPS      RBW"
+	print "                                             MB/s             MB/s"
+	print "=================================================================="
+	for j in sorted(jobs.values(), key=sortf, reverse=True):
+		dt = float(j.dt)
 		print "%-10s %-8s %-5s %6d %6d %9.2f %6d %9.2f" % (j.jobid.split(".")[0], j.owner, len(j.nodelist), j.miops/dt, j.wiops/dt, (j.wbw/dt)/1000000.0, j.riops/dt, (j.rbw/dt)/1000000.0)
+
+
+# print TOP like list of jobs, with absolute values over runtime
+def printJobSummary(fsname, key):
+	fs = filesystem(DBHOST, fsname)
+	jobs = fs.getRunningJobs()
+	jobs = fs.accumulateJobStats(jobs)
+	if key == "meta":
+		sortf=lambda x: x.miops	
+	elif key == "iops":
+		sortf=lambda x: x.wiops+x.riops
+	elif key == "bw":
+		sortf=lambda x: x.rbw+x.wbw
+	else:
+		print "use meta, iops or bw as sorting key"
+		sys.exit()
+
+	print "JOBID      OWNER    NODES  MIOPS   WIOPS     WBW   RIOPS      RBW"
+	print "                                             GB               GB"
+	print "=================================================================="
+	for j in sorted(jobs.values(), key=sortf, reverse=True):
+		print "%-10s %-8s %-5s %6d %6d %9.2f %6d %9.2f" % (j.jobid.split(".")[0], j.owner, len(j.nodelist), j.miops, j.wiops, j.wbw/1000000000.0, j.riops, j.rbw/1000000000.0)
+
+
+#printTopjobs(sys.argv[1], sys.argv[2])
+printJobSummary(sys.argv[1], sys.argv[2])
